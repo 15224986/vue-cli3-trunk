9373f1ebf551032613ebfe02ab2f0d0803239b70
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-09-11 13:23:38 +0800

    Update index.vue

--- src/views/container/index.vue
+++ src/views/container/index.vue
@@ -1,21 +1,29 @@
 <template>
     <el-container>
-    	<el-aside width="220px">
+    	<el-aside id="page-sidebar" width="auto">
+            <div class="neu-menu-tools">
+                <a @click="toggleSideBar()" class="hamburger" href="javascript:;">
+                    <i :class="sidebar.opened ? 'el-icon-s-unfold' : 'el-icon-s-fold'"></i>
+                </a>
+            </div>
             <el-menu
                 :default-active="$route.path"
                 class="el-menu-vertical-demo"
+                :collapse="sidebar.opened"
                 router
             >
                 <el-menu-item index="/anextUntil/login">
+                    <i class="el-icon-s-promotion"></i>
                     <span slot="title">登录</span>
                 </el-menu-item>
                 <el-menu-item index="/anextUntil/table">
+                    <i class="el-icon-s-order"></i>
                     <span slot="title">表格</span>
                 </el-menu-item>
                 <el-menu-item index="/anextUntil/form">
+                    <i class="el-icon-s-claim"></i>
                     <span slot="title">表单</span>
                 </el-menu-item>
-
     		</el-menu>
     	</el-aside>
         <el-main>
@@ -24,12 +32,19 @@
     </el-container>
 </template>
 <script>
+    import {
+        setSidebarOpened,
+        getSidebarOpened
+    } from '@/utils/auth'
 
     export default {
         mixins:[],
         components: {},
         data () {
 			return {
+                sidebar:{
+                    opened: getSidebarOpened() ? getSidebarOpened() : true
+                }
 			}
         },
         created(){
@@ -37,6 +52,14 @@
         mounted(){
         },
         methods:{
+            toggleSideBar() {
+                this.sidebar.opened = !this.sidebar.opened
+                if (this.sidebar.opened) {
+                   setSidebarOpened(true)
+                } else {
+                   setSidebarOpened(false)
+                }
+            }
         }
     }
 </script>
