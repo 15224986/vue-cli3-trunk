a0af01e7f818e79ba80c1ac5cd6e003b52d967e8
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-11-21 10:30:47 +0800

    Add files via upload

---
+++ src/api/container/table.js
@@ -0,0 +1,20 @@
+import request from '@/axios/request'
+
+export function getTableData(username, password) {
+    const params = {
+        userName: username,
+        password: password
+    }
+    return request({
+        url: '/mock/tableData',
+        method: 'post',
+        params
+    })
+}
+export function getTable(params) {
+    return request({
+        url: '/mock/table',
+        method: 'get',
+        params: params
+    })
+}
