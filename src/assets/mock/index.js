4aca032d55ac2377d68fe4513e129367a7c7f3f9
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-11-21 11:17:00 +0800

    Update index.js

--- src/assets/mock/index.js
+++ src/assets/mock/index.js
@@ -1,7 +1,7 @@
 const Mock = require('mockjs');
 
 //格式： Mock.mock( url, post/get , 返回的数据)；
-Mock.mock('/mock/table', 'get', require('./json/table'));
+Mock.mock('/mock/table?api=123', 'get', require('./json/table'));
 
 import tableData from './json/tableData.js'
-Mock.mock('/mock/tableData', 'get', tableData);
+Mock.mock('/mock/tableData', 'post', tableData);
