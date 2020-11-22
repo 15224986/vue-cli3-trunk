46884f17bad1ae93774dd6f4fa23b0ff06ab3f03
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-11-22 17:25:50 +0800

    Update main.vue

--- src/components/container/src/main.vue
+++ src/components/container/src/main.vue
@@ -1,20 +1,29 @@
 <template>
-	<section class="moc-container">
-        <slot></slot>
+	<section class="moc-container" :class="{'moc-horizontal': horizontal}">
+        <div v-if="bodier" class="moc-container-inner">
+            <slot></slot>
+        </div>
+        <slot v-else></slot>
     </section>
 </template>
 
 <script>
-
 	export default{
 		name: 'mocContainer',
 		componentName: 'mocContainer',
+        props: {
+            inner: {
+                type: Boolean,
+                default: false
+            },
+            horizontal: {
+                type: Boolean,
+                default: false
+            },
+        },
 		data() {
 			return{
 			}
-		},
-	  	props: {
-
 		}
 	}
 </script>
