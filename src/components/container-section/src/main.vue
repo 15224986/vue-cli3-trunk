5b0adb9870fa861d99111729ba33daa5d6bca093
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-11-22 17:27:24 +0800

    Update main.vue

--- src/components/container-section/src/main.vue
+++ src/components/container-section/src/main.vue
@@ -1,14 +1,27 @@
 <template>
     <section class="moc-container-section" :class="{ 'moc-container-bodier':bodier }">
-        <div class="moc-container-section-header" v-if="$scopedSlots.header">
-            <slot name="header"></slot>
+        <div v-if="bodier" class="moc-container-section-inner">
+            <div class="moc-container-section-header" v-if="$scopedSlots.header">
+                <slot name="header"></slot>
+            </div>
+            <div class="moc-container-section-body">
+                <slot></slot>
+            </div>
+            <div class="moc-container-section-footer" v-if="$scopedSlots.footer">
+                <slot name="footer"></slot>
+            </div>
         </div>
-        <div class="moc-container-section-body">
-            <slot></slot>
-        </div>
-        <div class="moc-container-section-footer" v-if="$scopedSlots.footer">
-            <slot name="footer"></slot>
-        </div>
+        <template v-else>
+            <div class="moc-container-section-header" v-if="$scopedSlots.header">
+                <slot name="header"></slot>
+            </div>
+            <div class="moc-container-section-body">
+                <slot></slot>
+            </div>
+            <div class="moc-container-section-footer" v-if="$scopedSlots.footer">
+                <slot name="footer"></slot>
+            </div>
+        </template>
     </section>
 </template>
 
@@ -16,14 +29,14 @@
 	export default{
 		name: 'mocSection',
 		componentName: 'mocSection',
-		data() {
-			return{}
-		},
 	  	props: {
             bodier: {
                 type: Boolean,
                 default: false
             },
+		},
+		data() {
+			return{}
 		},
         created() {
         },
