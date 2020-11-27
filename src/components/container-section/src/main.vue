8d157097050929d061d99a74446559f63367715c
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-11-27 16:08:30 +0800

    Update main.vue

--- src/components/container-section/src/main.vue
+++ src/components/container-section/src/main.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="moc-container-section" :class="{ 'moc-container-bodier':bodier }">
-        <div v-if="bodier" class="moc-container-section-inner">
+        <div class="moc-container-section-inner" :class="{'moc-is-scroll': isScroll}">
             <div class="moc-container-section-header" v-if="$scopedSlots.header">
                 <slot name="header"></slot>
             </div>
@@ -11,17 +11,6 @@
                 <slot name="footer"></slot>
             </div>
         </div>
-        <template v-else>
-            <div class="moc-container-section-header" v-if="$scopedSlots.header">
-                <slot name="header"></slot>
-            </div>
-            <div class="moc-container-section-body">
-                <slot></slot>
-            </div>
-            <div class="moc-container-section-footer" v-if="$scopedSlots.footer">
-                <slot name="footer"></slot>
-            </div>
-        </template>
     </section>
 </template>
 
@@ -34,6 +23,10 @@
                 type: Boolean,
                 default: false
             },
+            isScroll: {
+                type: Boolean,
+                default: false
+            }
 		},
 		data() {
 			return{}
