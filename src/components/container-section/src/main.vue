b53fecf658ea3964195225640284f45ba3bc8361
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-12-02 13:44:07 +0800

    Update main.vue

--- src/components/container-section/src/main.vue
+++ src/components/container-section/src/main.vue
@@ -1,16 +1,23 @@
 <template>
-    <section class="moc-container-section" :class="{ 'moc-container-bodier':bodier }">
-        <div class="moc-container-section-inner" :class="{'moc-is-scroll': isScroll}">
+    <section class="moc-container-section" :class="{ 'is-bodier':bodier }">
+        <div v-if="bodier" class="moc-container-section-inner">
             <div class="moc-container-section-header" v-if="$scopedSlots.header">
                 <slot name="header"></slot>
             </div>
-            <div class="moc-container-section-body">
-                <slot></slot>
-            </div>
+            <slot></slot>
             <div class="moc-container-section-footer" v-if="$scopedSlots.footer">
                 <slot name="footer"></slot>
             </div>
         </div>
+        <template v-else>
+            <div class="moc-container-section-header" v-if="$scopedSlots.header">
+                <slot name="header"></slot>
+            </div>
+            <slot></slot>
+            <div class="moc-container-section-footer" v-if="$scopedSlots.footer">
+                <slot name="footer"></slot>
+            </div>
+        </template>
     </section>
 </template>
 
@@ -20,10 +27,6 @@
 		componentName: 'mocSection',
 	  	props: {
             bodier: {
-                type: Boolean,
-                default: false
-            },
-            isScroll: {
                 type: Boolean,
                 default: false
             }
