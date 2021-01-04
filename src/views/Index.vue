5e30d6f7c4a09754b88362eefd752d9070117b05
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-01-04 15:49:32 +0800

    Update Index.vue

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,8 +1,11 @@
 <template>
     <el-container id="page-container">
-		<el-header id="page-header">
+        <el-header id="page-header">
             <div class="moc-page-nav">
-                <router-link :to="{path:'/'}">首页</router-link>
+                <router-link :to="{path:'/index'}" class="moc-page-nav-item">首頁</router-link>
+                <router-link :to="{path:'/anextUntil'}" class="moc-page-nav-item">数据管理</router-link>
+                <router-link :to="{path:'/contianer'}" class="moc-page-nav-item">资产管理</router-link>
+                <router-link :to="{path:'/content'}" class="moc-page-nav-item">安全管理</router-link>
             </div>
         </el-header>
         <router-view></router-view>
@@ -10,15 +13,11 @@
 </template>
 <script>
     export default {
-        data () {
-			return {
-			}
+        data() {
+            return {}
         },
-        created(){
-        },
-        mounted(){
-        },
-        methods:{
-        }
+        created() {},
+        mounted() {},
+        methods: {}
     }
 </script>
