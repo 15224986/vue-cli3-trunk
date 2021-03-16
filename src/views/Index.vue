40ebdef59b28190824aaffb4d6f410ebc7e216a3
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-03-16 17:31:30 +0800

    Update Index.vue

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,10 +2,10 @@
     <el-container id="page-container">
         <el-header id="page-header">
             <div class="moc-page-nav">
-                <router-link :to="{path:'/index'}" class="moc-page-nav-item">首頁</router-link>
-                <router-link :to="{path:'/anextUntil'}" class="moc-page-nav-item">数据管理</router-link>
-                <router-link :to="{path:'/contianer'}" class="moc-page-nav-item">资产管理</router-link>
-                <router-link :to="{path:'/content'}" class="moc-page-nav-item">安全管理</router-link>
+                <router-link :to="{path:'/home'}" class="moc-page-nav-item">首頁</router-link>
+                <router-link :to="{path:'/contianer'}" class="moc-page-nav-item">基础页面</router-link>
+                <router-link :to="{path:'/assets'}" class="moc-page-nav-item">资产管理</router-link>
+                <router-link :to="{path:'/safety'}" class="moc-page-nav-item">安全管理</router-link>
             </div>
         </el-header>
         <router-view></router-view>
