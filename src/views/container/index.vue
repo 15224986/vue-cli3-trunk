60af0494af53a9c9d9591a0581839380e148210b
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-03-16 17:32:34 +0800

    Update index.vue

--- src/views/container/index.vue
+++ src/views/container/index.vue
@@ -12,22 +12,22 @@
                 :collapse="sidebar.opened"
                 router
             >
-                <el-menu-item index="/anextUntil/login">
+                <el-menu-item index="/contianer/login">
                     <i class="el-icon-s-promotion"></i>
                     <span slot="title">登录</span>
                 </el-menu-item>
-                <el-menu-item index="/anextUntil/table">
+                <el-menu-item index="/contianer/table">
                     <i class="el-icon-s-order"></i>
                     <span slot="title">表格</span>
                 </el-menu-item>
-                <el-menu-item index="/anextUntil/form">
+                <el-menu-item index="/contianer/form">
                     <i class="el-icon-s-claim"></i>
                     <span slot="title">表单</span>
                 </el-menu-item>
     		</el-menu>
     	</el-aside>
-        <el-main id="page-content">
-            <transition  name="fade-transform" mode="out-in">
+        <el-main id="page-content" class="stage-animation">
+            <transition  name="stage-left-right" mode="out-in">
                 <router-view></router-view>
             </transition>
     	</el-main>
@@ -62,21 +62,22 @@
     }
 </script>
 <style lang="scss">
-    #page-content{
+    .stage-animation{
         overflow: hidden
     }
     /* 可以设置不同的进入和离开动画 */
     /* 设置持续时间和动画函数 */
-    .fade-transform-enter-active,.fade-transform-leave-active {
+    .stage-left-right-enter-active,
+    .stage-left-right-leave-active {
         -webkit-transition: all .5s;
         transition: all .5s
     }
-    .fade-transform-enter {
+    .stage-left-right-enter {
         opacity: 0;
         -webkit-transform: translateX(-30px);
         transform: translateX(-30px)
     }
-    .fade-transform-leave-to {
+    .stage-left-right-leave-to {
         opacity: 0;
         -webkit-transform: translateX(30px);
         transform: translateX(30px)
