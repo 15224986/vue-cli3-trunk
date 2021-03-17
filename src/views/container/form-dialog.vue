96de02372d9f1759565829a51f664fc619d09f55
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-03-17 16:06:13 +0800

    Add files via upload

---
+++ src/views/container/form-dialog.vue
@@ -0,0 +1,79 @@
+<template>
+    <el-dialog
+        title="系统提示"
+        :visible="visible"
+        width="960px"
+        top="50px"
+        append-to-body
+        v-mocDialogDrag
+        custom-class="moc-dialog-fixed"
+        @close="onCancel()"
+    >
+        <moc-container>
+            <moc-section class="project-form">
+                <p>典型表单</p>
+                <el-form ref="formHorizontal" :model="form" label-width="80px" class="moc-form-horizontal">
+                    <el-form-item v-for="item in conts" :key="item" label="活动名称">
+                        <el-input v-model="form.name"></el-input>
+                    </el-form-item>
+                </el-form>
+                <p>行内表单</p>
+                <el-form ref="formInline" :model="form" label-width="80px" :inline="true" class="moc-form-inline">
+                    <el-form-item v-for="item in conts" :key="item" label="活动名称">
+                        <el-input v-model="form.name"></el-input>
+                    </el-form-item>
+                </el-form>
+            </moc-section>
+        </moc-container>
+        <template #footer>
+            <el-button @click="onSubmit()">取 消</el-button>
+            <el-button type="primary" @click="onCancel()">确 定</el-button>
+        </template>
+    </el-dialog>
+
+</template>
+<script>
+    /**
+     * 混入对象
+     */
+    import common from '@/mixins/common.js';            // 通用  每个页面都需要引入
+
+    export default {
+        mixins:[ common ],
+        components: {},
+        props: {
+            visible: {
+                type: Boolean,
+                default: false
+            }
+        },
+        data () {
+			return {
+                /**
+                 * 表单
+                 */
+                conts: 20,
+                form:{
+                    name:''
+                }
+			}
+        },
+        created(){
+
+        },
+        mounted(){
+
+        },
+        methods:{
+            /**
+             * 确定、取消
+             */
+            onSubmit(){
+                this.$emit('after-callback', true);
+            },
+            onCancel(){
+                this.$emit('after-callback', true);
+            },
+        }
+    }
+</script>
