822290928c2041bbd0af0b76ab1875338752c711
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-03-17 16:05:03 +0800

    Update table-dialog.vue

--- src/views/container/table-dialog.vue
+++ src/views/container/table-dialog.vue
@@ -1,4 +1,15 @@
-<moc-container flex>
+<template>
+    <el-dialog
+        title="系统提示"
+        :visible="visible"
+        width="88%"
+        top="50px"
+        append-to-body
+        v-mocDialogDrag
+        custom-class="moc-dialog-fixed"
+        @close="onCancel()"
+    >
+        <moc-container flex>
             <moc-section class="project-search">
                 <el-form :model="search" :inline="true" label-width="120px" label-suffix="：">
                     <el-form-item label="审批人">
@@ -88,3 +99,161 @@
                 </el-pagination>
             </moc-section>
         </moc-container>
+        <template #footer>
+            <el-button @click="onSubmit()">取 消</el-button>
+            <el-button type="primary" @click="onCancel()">确 定</el-button>
+        </template>
+    </el-dialog>
+
+</template>
+<script>
+    /**
+     * 混入对象
+     */
+    // 页面通用的
+    import common from '@/mixins/common.js';
+    // 表格通用的
+    import tableCommon from '@/mixins/tableCommon.js';
+    // 表格数据格式化
+    import tableFormatter from '@/mixins/tableFormatter.js';
+    /**
+     * 公共数据、方法的几种使用方式
+     * 1.公共数据、方法写在mixins文件夹里，以mixin的形式使用
+     * 2.公共数据写config文件夹中,用到的次数较多的，写在global.js里，全局引入
+     * 3.公共方法写在常用的写在'assets/scripts/common.js'里
+     * 4.不常用的写在utils里面，按需引入
+     */
+    export default {
+        mixins:[ common, tableCommon, tableFormatter ],
+        components: {},
+        props: {
+            visible: {
+                type: Boolean,
+                default: false
+            },
+            groupId: {
+                type: [ Boolean, String, Number],
+                default: '1'
+            }
+        },
+        data () {
+            const item = {
+				date: '20160502000000',
+				name: '王小虎',
+				address: '上海市普陀区金沙江路 1518 弄'
+			};
+			return {
+                /**
+                 * 搜索条件
+                 */
+                search: {
+                    user: '',
+                    region: '',
+                    region2: '',
+                    date: '',
+                    datetime: '',
+                    checkTime: '',
+                    arr:[1,2,3],
+                    obj:{
+                        a:1,
+                        b:22222
+                    },
+                },
+                /**
+                 * 表格
+                 */
+                calcTableHeightDom:"#project-dialog-table",
+				tableData: Array(20).fill(item),
+                options:{
+                    region:[
+                        {
+                            label: '黄金糕',
+                            value: '选项1'
+                        },
+                        {
+                            label: '双皮奶',
+                            value: '选项2',
+                            disabled: true
+                        },
+                        {
+                            label: '蚵仔煎',
+                            value: '选项3'
+                        },
+                        {
+                            label: '蚵仔煎',
+                            value: '选项4'
+                        },
+                        {
+                            label: '蚵仔煎',
+                            value: '选项5'
+                        },
+                        {
+                            label: '蚵仔煎',
+                            value: '选项6'
+                        },
+                        {
+                            label: '蚵仔煎',
+                            value: '选项7'
+                        },
+                        {
+                            label: '蚵仔煎',
+                            value: '选项8'
+                        },
+                        {
+                            label: '蚵仔煎',
+                            value: '选项9'
+                        }
+                    ]
+                }
+			}
+        },
+        created(){
+
+        },
+        mounted () {
+
+        },
+        methods:{
+            /**
+             * 初始化数据，在父页面调用
+             */
+            inintData(){
+                console.log('初始化弹框数据', this.groupId)
+                this.calcTableHeight();
+            },
+            /**
+             * 搜索事件
+             */
+            onSearch(){
+                this.pagination.current = 1;
+                this.initTableData();
+            },
+
+            /**
+             * 确定、取消
+             */
+            onSubmit(){
+                this.$emit('after-callback', true);
+            },
+            onCancel(){
+                this.$emit('after-callback', true);
+            },
+
+            /**
+             * 下拉框 options 的数据
+             */
+            initOptions(){
+                /**
+                 * 模拟请求数据
+                 */
+                this.initTableData();
+            },
+            /**
+             * 初始化表格数据
+             */
+            initTableData() {
+                let params = { ...this.$lodash.cloneDeep(this.search), ...this.pagination };
+            },
+        }
+    }
+</script>
