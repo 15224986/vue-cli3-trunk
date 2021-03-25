baed3556527835af3db309f49f7b6ad9264e0f87
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-03-25 13:46:58 +0800

    Update dialogDrag.js

--- src/directives/modules/dialogDrag.js
+++ src/directives/modules/dialogDrag.js
@@ -1,4 +1,7 @@
-// el-dialog 拖拽
+/**
+ * el-dialog 拖拽效果
+ * value  如果是 true 则开边缘检测，否则只开启顶部检测
+ */
 export default {
     bind(el, binding, vnode, oldVnode) {
         const dialogHeaderEl = el.querySelector('.el-dialog__header');
@@ -6,13 +9,12 @@ export default {
         dialogHeaderEl.style.cursor = 'move';
         dragDom.classList.add('moc-drag-dialog');
         // 获取原有属性 ie dom元素.currentStyle 火狐谷歌 window.getComputedStyle(dom元素, null);
-        const sty = dragDom.currentStyle || window.getComputedStyle(dragDom, null);
+        const sty = window.getComputedStyle(dragDom, null) || dragDom.currentStyle;
 
         dialogHeaderEl.onmousedown = (e) => {
             // 鼠标按下，计算当前元素距离可视区的距离
             const disX = e.clientX - dialogHeaderEl.offsetLeft,
                   disY = e.clientY - dialogHeaderEl.offsetTop;
-
             // 获取到的值带px 正则匹配替换
             let styL,
                 styT;
@@ -24,73 +26,44 @@ export default {
                 styL = +sty.left.replace(/\px/g, '');
                 styT = +sty.top.replace(/\px/g, '');
             };
-
             document.onmousemove = function (e) {
                 // 通过事件委托，计算移动的距离
                 const l = e.clientX - disX;
                 const t = e.clientY - disY;
-
-                // 获取弹出框距离顶部和左侧的距离
-                let dragDomX = dragDom.offsetLeft,
-                    dragDomY = dragDom.offsetTop;
                 // 计算移动的距离
                 let moveX = l + styL,
-                    moveY = t + styT,
-                    top = sty.top.replace(/\px/g, '')-0,
-                    left = sty.left.replace(/\px/g, '')-0;
-                /**
-                 * 边缘检测
-                 */
-                if(dragDomX<=0&&moveX<left){
-                    moveX = left;
-                }
-                if(dragDomY<=0 && moveY<top){
-                    moveY = top;
-                }
+                    moveY = t + styT;
                 // 移动当前元素
                 dragDom.style.left = `${moveX}px`;
                 dragDom.style.top = `${moveY}px`;
             };
-
             document.onmouseup = function (e) {
                 /**
-                 * 获取当前位置
+                 * 边缘检测
                  */
-                let left =  dragDom.offsetLeft,
-                    top =  dragDom.offsetTop,
-                    styL = sty.left.replace(/\px/g, '')-0,
-                    styT = sty.top.replace(/\px/g, '')-0;
-                /**
-                 * 左侧、顶部 边缘检测
-                 */
-                if( left < 0){
-                    dragDom.style.left = `${styL - left}px`;
-                }
+                let top =  dragDom.offsetTop,
+                   styT = sty.top.replace(/\px/g, '')-0;
                 if( top < 0){
                     dragDom.style.top = `${styT - top}px`;
                 }
-                /**
-                 * 右侧、底部 边缘检测
-                 */
-                let width = dragDom.offsetWidth,
-                    height = dragDom.offsetHeight,
-                    winWidth = document.body.offsetWidth,
-                    winHeight = document.body.offsetHeight;
-                // 重新获取当前位置
-                left =  dragDom.offsetLeft;
-                top =  dragDom.offsetTop;
-                styL = sty.left.replace(/\px/g, '')-0;
-                styT = sty.top.replace(/\px/g, '')-0;
-                /**
-                 * 重新定位
-                 */
-                let moveX = winWidth - left - width;
-                if( moveX < 0 ){
-                    dragDom.style.left = `${ styL + moveX }px`;
-                }
-                let moveY = winHeight - top - height;
-                if( moveY < 0 ){
-                    dragDom.style.top = `${ styT + moveY }px`;
+                if( binding.value === true ){
+                    let left =  dragDom.offsetLeft,
+                        styL = sty.left.replace(/\px/g, '')-0,
+                        width = dragDom.offsetWidth,
+                        height = dragDom.offsetHeight,
+                        winWidth = document.body.clientWidth,
+                        winHeight = document.body.clientHeight;
+                    if( left < 0){
+                        dragDom.style.left = `${styL - left}px`;
+                    }
+                    let moveX = winWidth - left - width;
+                    if( moveX < 0 ){
+                        dragDom.style.left = `${ styL + moveX }px`;
+                    }
+                    let moveY = winHeight - top - height;
+                    if( moveY < 0 ){
+                        dragDom.style.top = `${ styT + moveY }px`;
+                    }
                 }
                 document.onmousemove = null;
                 document.onmouseup = null;
