679a255a1d687dc2f35ff06fc18c8e88f4c8bf74
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-03-26 16:06:20 +0800

    Add files via upload

---
+++ src/views/assets/index.vue
@@ -0,0 +1,78 @@
+<template>
+    <el-container id="page-bodier">
+    	<el-aside id="page-sidebar" width="auto">
+            <div class="neu-menu-tools">
+                <a @click="toggleSideBar()" class="hamburger" href="javascript:;">
+                    <i :class="sidebar.opened ? 'el-icon-s-unfold' : 'el-icon-s-fold'"></i>
+                </a>
+            </div>
+            <el-menu
+                :default-active="$route.path"
+                class="el-menu-vertical-demo"
+                :collapse="sidebar.opened"
+                router
+            >
+                <el-menu-item index="/assets/standard">
+                    <i class="el-icon-s-promotion"></i>
+                    <span slot="title">内部添加tabs</span>
+                </el-menu-item>
+                <el-menu-item index="/assets/strategy">
+                    <i class="el-icon-s-promotion"></i>
+                    <span slot="title">外部添加tabs</span>
+                </el-menu-item>
+    		</el-menu>
+    	</el-aside>
+        <el-main id="page-content">
+            <transition  name="stage-left-right" mode="out-in">
+                <router-view></router-view>
+            </transition>
+    	</el-main>
+    </el-container>
+</template>
+<script>
+    import {
+        setSidebarOpened,
+        getSidebarOpened
+    } from '@/utils/auth'
+
+    export default {
+        mixins:[],
+        components: {},
+        data () {
+			return {
+                sidebar:{
+                    opened: getSidebarOpened()
+                }
+			}
+        },
+        created(){
+        },
+        mounted(){
+        },
+        methods:{
+            toggleSideBar() {
+                this.sidebar.opened = !this.sidebar.opened
+                setSidebarOpened(this.sidebar.opened)
+            }
+        }
+    }
+</script>
+<style lang="scss">
+    /* 可以设置不同的进入和离开动画 */
+    /* 设置持续时间和动画函数 */
+    .stage-left-right-enter-active,
+    .stage-left-right-leave-active {
+        -webkit-transition: all .5s;
+        transition: all .5s
+    }
+    .stage-left-right-enter {
+        opacity: 0;
+        -webkit-transform: translateX(-30px);
+        transform: translateX(-30px)
+    }
+    .stage-left-right-leave-to {
+        opacity: 0;
+        -webkit-transform: translateX(30px);
+        transform: translateX(30px)
+    }
+</style>
