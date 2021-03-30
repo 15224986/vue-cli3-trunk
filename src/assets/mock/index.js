33b9701c177441496eb5caa72134d55eea910f71
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-03-30 20:42:05 +0800

    Update index.js

--- src/assets/mock/index.js
+++ src/assets/mock/index.js
@@ -1,5 +1,13 @@
 const Mock = require('mockjs');
 
+/**
+ * 请求拦截
+ */
+Mock.setup({
+    timeout: "500-3000"
+})
+
+
 //格式： Mock.mock( url, post/get , 返回的数据)；
 Mock.mock('/mock/table?api=123', 'get', require('./json/table'));
 
