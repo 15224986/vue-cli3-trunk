b24da557025bfbdc0b63b327d6ba8267a5ad769c
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-04-01 11:11:56 +0800

    Update assets.js

--- src/router/modules/assets.js
+++ src/router/modules/assets.js
@@ -1,24 +1,18 @@
-export default {
-    path: 'assets',
-    name: 'assets',
-    component: () => import(/* webpackChunkName: "assets" */ '@/views/assets/index'),
-	redirect: '/assets/standard',
-	children: [
-		{
-			path: 'standard',
-			name: 'standard',
-			component: () => import(/* webpackChunkName: "assets" */ '@/views/assets/standard'),
-			meta: {
-				title: '内部添加tabs'
-			}
-		},
-        {
-        	path: 'strategy',
-        	name: 'strategy',
-        	component: () => import(/* webpackChunkName: "assets" */ '@/views/assets/strategy'),
-        	meta: {
-        		title: '外部添加tabs'
-        	}
+export default [
+    {
+        path: 'standard',
+        name: 'standard',
+        component: () => import(/* webpackChunkName: "assets" */ '@/views/assets/standard'),
+        meta: {
+            title: '内部添加tabs'
         }
-	]
-}
+    },
+    {
+        path: 'strategy',
+        name: 'strategy',
+        component: () => import(/* webpackChunkName: "assets" */ '@/views/assets/strategy'),
+        meta: {
+            title: '外部添加tabs'
+        }
+    }
+]
