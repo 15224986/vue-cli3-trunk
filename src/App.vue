c05d21fb46920a94a22d4273f2518252b69f1d23
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-04-01 10:57:34 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
-    <div id="app">
-        <router-view/>
-    </div>
+    <el-container id="app">
+        <el-header id="page-header">
+            <div class="moc-page-logo">XX管理系统</div>
+            <div class="moc-page-nav">
+                <router-link :to="{path:'/home'}" class="moc-page-nav-item">首頁</router-link>
+                <router-link :to="{path:'/contianer'}" class="moc-page-nav-item">基础页面</router-link>
+                <router-link :to="{path:'/assets'}" class="moc-page-nav-item">资产管理</router-link>
+                <router-link :to="{path:'/safety'}" class="moc-page-nav-item">安全管理</router-link>
+            </div>
+        </el-header>
+        <router-view></router-view>
+    </el-container>
 </template>
 <script>
     // 加载资源
