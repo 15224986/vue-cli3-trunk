345b7c1d890680dfc037403791a4ddfc123715a0
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-04-15 15:48:35 +0800

    Update common.js

--- src/mixins/common.js
+++ src/mixins/common.js
@@ -7,31 +7,6 @@ export default {
             mainLoading: false,                 // 页面的加载状态
 		}
 	},
-    filters:{
-        /**
-         * 通过过滤，将select的value转化为label
-         */
-        selectFormatter( value, options, type ){
-            if( type === "array" ){
-                if( !Array.isArray(value) ){
-                    value = value.split(',')
-                }
-                let valueText = [];
-                options.forEach(element => {
-                    if( value.includes(element.value) ){
-                        valueText.push( element.label )
-                    }
-                });
-                return valueText.join()
-            }else{
-                options.forEach(element => {
-                    if( value === element.value ){
-                        return element.label
-                    }
-                });
-            }
-        }
-    },
 	methods: {
 		$loadDom(callback, times=500){
 			this.$nextTick(function () {
