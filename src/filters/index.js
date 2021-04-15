263580c5b64403bc960442ba9ea477378e9e8fec
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-04-15 15:21:21 +0800

    Update index.js

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,4 +1,31 @@
 /**
+ * 通过过滤，将select的value转化为label
+ * @param {string} value
+ * @return {string}
+ */
+export function selectFormatter(value, options, type) {
+    if( type === "array" ){
+        if( !Array.isArray(value) ){
+            value = value.split(',')
+        }
+        let valueText = [];
+        options.forEach(element => {
+            if( value.includes(element.value) ){
+                valueText.push( element.label )
+            }
+        });
+        return valueText.join()
+    }else{
+        options.forEach(element => {
+            if( value === element.value ){
+                return element.label
+            }
+        });
+    }
+}
+
+
+/**
  * 首字母大写
  * @param {string} value
  * @return {string}
