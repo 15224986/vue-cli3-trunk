d0267174b659976484067da1e9ef2a18b9c7d1ac
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-04-15 15:20:38 +0800

    Update tableFormatter.js

--- src/mixins/tableFormatter.js
+++ src/mixins/tableFormatter.js
@@ -1,68 +1,19 @@
 export default {
-	methods:{
-        /**
-         * 表格对应select格式化
-         */
-        selectFormatter( value, options, type ){
-            if( type === "array" ){
-                if( !Array.isArray(value) ){
-                    value = value.split(',')
-                }
-                let valueText = [];
-                options.forEach(element => {
-                    if( value.includes(element.value) ){
-                        valueText.push( element.label )
-                    }
-                });
-                return valueText.join()
-            }else{
-                options.forEach(element => {
-                    if( value === element.value ){
-                        return element.label
-                    }
-                });
-            }
-        },
-		/**
-		 * 格式化表格里面的时间日期结构
-		 */
-		dateFormatter(row, column, cellValue, index){
-			if(cellValue){
-				cellValue = cellValue + '';
-				let langthNum = cellValue.length;
-				var year = '',
-					month = '',
-					day = '',
-					hour = '',
-					minute = '',
-					second = '',
-					date = '';
-				if( langthNum >= 4 ){
-					year = cellValue.slice(0,4);
-					date = year;
-				}
-				if(langthNum >= 6){
-					month = cellValue.slice(4,6);
-					date += '-' + month;
-				}
-				if(langthNum >= 8){
-					day = cellValue.slice(6,8);
-					date += '-' + day;
-				}
-				if(langthNum >= 10){
-					hour = cellValue.slice(8,10);
-					date += ' ' + hour;
-				}
-				if(langthNum >= 12){
-					minute = cellValue.slice(10,12);
-					date += ':' + minute;
-				}
-				if(langthNum >= 14){
-					second = cellValue.slice(12,14);
-					date += ':' + second;
-				}
-				return date;
-			}
+	options() {
+		return {
+            /**
+             * loading
+             */
+            mainLoading: false,                 // 页面的加载状态
+		}
+	},
+	methods: {
+		$loadDom(callback, times=500){
+			this.$nextTick(function () {
+				setTimeout(()=>{
+					callback && typeof(callback)==="function" ? callback.call(this) : ""
+				}, times);
+			});
 		},
 	}
 }
