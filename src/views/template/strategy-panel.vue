3b92e51703ce4b4755e8cd685599fb2ff9b563af
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-05-10 10:29:23 +0800

    Add files via upload

---
+++ src/views/template/strategy-panel.vue
@@ -0,0 +1,31 @@
+<template>
+    <moc-container flex>
+        <moc-section class="project-toolbar m-t-sm">
+            <dl>
+                <dt>{{title}}</dt>
+                <dd>{{text}}</dd>
+            </dl>
+        </moc-section>
+    </moc-container>
+</template>
+<script>
+
+    export default {
+        props:{
+            title: '',
+            text: '',
+        },
+        data () {
+			return {}
+        },
+        created(){
+
+        },
+        mounted () {
+
+        },
+        methods:{
+
+        }
+    }
+</script>
