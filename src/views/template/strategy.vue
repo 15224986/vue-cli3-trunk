1c25df80176f4ab38a2e8b159101ddf070e1e28d
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-05-21 16:46:54 +0800

    Update strategy.vue

--- src/views/template/strategy.vue
+++ src/views/template/strategy.vue
@@ -1,19 +1,20 @@
 <template>
-    <el-tabs v-model="activeName" @tab-click="handleClick" class="moc-container-tabs" id="page-content" type="border-card">
-        <el-tab-pane label="用户管理" name="first">
-            <strategy-first></strategy-first>
-        </el-tab-pane>
-        <el-tab-pane label="配置管理" name="second">
-            <strategy-panel v-if="activeName=='second'" title="配置管理" text="每次进入需要重新 初始化"></strategy-panel>
-        </el-tab-pane>
-        <el-tab-pane label="角色管理" name="third">
-            <strategy-panel v-if="third" title="角色管理" text="不需要重新 初始化"></strategy-panel>
-        </el-tab-pane>
-        <el-tab-pane label="定时任务补偿" name="fourth">
-            <strategy-panel v-if="fourth" title="定时任务补偿" text="不需要重新 初始化"></strategy-panel>
-        </el-tab-pane>
-    </el-tabs>
-
+    <article id="page-container">
+        <el-tabs v-model="activeName" @tab-click="handleClick" class="moc-container-tabs" id="page-content" type="border-card">
+            <el-tab-pane label="用户管理" name="first">
+                <strategy-first></strategy-first>
+            </el-tab-pane>
+            <el-tab-pane label="配置管理" name="second">
+                <strategy-panel v-if="activeName=='second'" title="配置管理" text="每次进入需要重新 初始化"></strategy-panel>
+            </el-tab-pane>
+            <el-tab-pane label="角色管理" name="third">
+                <strategy-panel v-if="third" title="角色管理" text="不需要重新 初始化"></strategy-panel>
+            </el-tab-pane>
+            <el-tab-pane label="定时任务补偿" name="fourth">
+                <strategy-panel v-if="fourth" title="定时任务补偿" text="不需要重新 初始化"></strategy-panel>
+            </el-tab-pane>
+        </el-tabs>
+    </article>
 </template>
 <script>
 
