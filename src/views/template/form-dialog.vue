bd51de5662985d0d21dfe317a940f168b08c5918
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-06-04 10:39:46 +0800

    Update form-dialog.vue

--- src/views/template/form-dialog.vue
+++ src/views/template/form-dialog.vue
@@ -1,114 +1,107 @@
 <template>
-    <el-dialog title="系统提示" :visible="visible" width="960px" fullscreen @close="onCancel()">
-        <moc-container class="project-dialog-content">
-            <moc-section class="project-form">
-                <p>典型验证</p>
-                <el-form :model="ruleForm" ref="ruleForm" label-width="100px" class="project-form-horizontal" label-suffix="：">
-                    <el-form-item
-                        label="活动名称"
-                        prop="name"
-                        :rules="[
-                            { required: true, message: '请输入活动名称', trigger: 'blur' },
-                            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
-                        ]"
-                    >
-                        <el-input v-model="ruleForm.name"></el-input>
-                    </el-form-item>
-                    <el-form-item
-                        label="活动区域"
-                        prop="region"
-                        :rules="{ required: true, message: '请选择活动区域', trigger: 'change' }"
-                    >
-                        <el-select v-model="ruleForm.region" placeholder="请选择活动区域">
-                            <el-option label="区域一" value="shanghai"></el-option>
-                            <el-option label="区域二" value="beijing"></el-option>
-                        </el-select>
-                    </el-form-item>
-                    <el-form-item label="活动时间" required>
-                        <el-col :span="11">
-                            <el-form-item
-                                prop="date1"
-                                :rules="{ type: 'date', required: true, message: '请选择日期', trigger: 'change' }"
-                            >
-                                <el-date-picker type="date" placeholder="选择日期" v-model="ruleForm.date1"></el-date-picker>
-                            </el-form-item>
-                        </el-col>
-                        <el-col class="line" :span="2">-</el-col>
-                        <el-col :span="11">
-                            <el-form-item
-                                prop="date2"
-                                :rules="{ type: 'date', required: true, message: '请选择时间', trigger: 'change' }"
-                            >
-                                <el-time-picker placeholder="选择时间" v-model="ruleForm.date2"></el-time-picker>
-                            </el-form-item>
-                        </el-col>
-                    </el-form-item>
-                    <el-form-item
-                        label="即时配送"
-                        prop="delivery"
-                    >
-                        <el-switch v-model="ruleForm.delivery"></el-switch>
-                    </el-form-item>
-                    <el-form-item
-                        label="活动性质"
-                        prop="type"
-                        :rules="{ required: true, message: '请至少选择一个活动性质', trigger: 'change' }"
-                    >
-                        <el-checkbox-group v-model="ruleForm.type">
-                            <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-                            <el-checkbox label="地推活动" name="type"></el-checkbox>
-                            <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-                            <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-                        </el-checkbox-group>
-                    </el-form-item>
-                    <el-form-item
-                        label="特殊资源"
-                        prop="resource"
-                        :rules="{ required: true, message: '请选择活动资源', trigger: 'change' }"
-                    >
-                        <el-radio-group v-model="ruleForm.resource">
-                            <el-radio label="线上品牌商赞助"></el-radio>
-                            <el-radio label="线下场地免费"></el-radio>
-                        </el-radio-group>
-                    </el-form-item>
-                    <el-form-item
-                        label="活动形式"
-                        prop="desc"
-                        :rules="{ required: true, message: '请填写活动形式', trigger: 'blur' }"
-                    >
-                        <el-input type="textarea" v-model="ruleForm.desc"></el-input>
-                    </el-form-item>
-                    <el-form-item>
-                        <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
-                        <el-button @click="resetForm('ruleForm')">重置</el-button>
-                    </el-form-item>
-                </el-form>
-                <p>典型表单</p>
-                <el-form ref="formHorizontal" :model="form" label-width="100px" class="project-form-horizontal" label-suffix="：">
-                    <el-form-item v-for="item in conts" :key="item" label="活动名称">
-                        <el-input v-model="form.name"></el-input>
-                    </el-form-item>
-                </el-form>
-                <p>行内表单</p>
-                <el-form ref="formInline" :model="form" label-width="100px" :inline="true" class="project-form-inline" label-suffix="：">
-                    <el-form-item v-for="item in conts" :key="item" label="活动名称">
-                        <el-input v-model="form.name"></el-input>
-                    </el-form-item>
-                </el-form>
-                <p>查看详情</p>
-                <el-form ref="formInline" :model="form" label-width="100px" :inline="true" class="project-form-details content-width-180" label-suffix="：">
-                    <el-form-item v-for="item in conts" :key="item" label="活动名称">
-                        <p class="project-form-text">{{form.name}}</p>
-                    </el-form-item>
-                </el-form>
-            </moc-section>
-        </moc-container>
-        <template #footer>
-            <el-button @click="onSubmit()">取 消</el-button>
-            <el-button type="primary" @click="onCancel()">确 定</el-button>
-        </template>
-    </el-dialog>
-
+    <moc-container class="project-dialog-content">
+        <moc-section class="project-form">
+            <p>典型验证</p>
+            <el-form :model="ruleForm" ref="ruleForm" label-width="100px" class="project-form-horizontal" label-suffix="：">
+                <el-form-item
+                    label="活动名称"
+                    prop="name"
+                    :rules="[
+                        { required: true, message: '请输入活动名称', trigger: 'blur' },
+                        { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+                    ]"
+                >
+                    <el-input v-model="ruleForm.name"></el-input>
+                </el-form-item>
+                <el-form-item
+                    label="活动区域"
+                    prop="region"
+                    :rules="{ required: true, message: '请选择活动区域', trigger: 'change' }"
+                >
+                    <el-select v-model="ruleForm.region" placeholder="请选择活动区域">
+                        <el-option label="区域一" value="shanghai"></el-option>
+                        <el-option label="区域二" value="beijing"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="活动时间" required>
+                    <el-col :span="11">
+                        <el-form-item
+                            prop="date1"
+                            :rules="{ type: 'date', required: true, message: '请选择日期', trigger: 'change' }"
+                        >
+                            <el-date-picker type="date" placeholder="选择日期" v-model="ruleForm.date1"></el-date-picker>
+                        </el-form-item>
+                    </el-col>
+                    <el-col class="line" :span="2">-</el-col>
+                    <el-col :span="11">
+                        <el-form-item
+                            prop="date2"
+                            :rules="{ type: 'date', required: true, message: '请选择时间', trigger: 'change' }"
+                        >
+                            <el-time-picker placeholder="选择时间" v-model="ruleForm.date2"></el-time-picker>
+                        </el-form-item>
+                    </el-col>
+                </el-form-item>
+                <el-form-item
+                    label="即时配送"
+                    prop="delivery"
+                >
+                    <el-switch v-model="ruleForm.delivery"></el-switch>
+                </el-form-item>
+                <el-form-item
+                    label="活动性质"
+                    prop="type"
+                    :rules="{ required: true, message: '请至少选择一个活动性质', trigger: 'change' }"
+                >
+                    <el-checkbox-group v-model="ruleForm.type">
+                        <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
+                        <el-checkbox label="地推活动" name="type"></el-checkbox>
+                        <el-checkbox label="线下主题活动" name="type"></el-checkbox>
+                        <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
+                    </el-checkbox-group>
+                </el-form-item>
+                <el-form-item
+                    label="特殊资源"
+                    prop="resource"
+                    :rules="{ required: true, message: '请选择活动资源', trigger: 'change' }"
+                >
+                    <el-radio-group v-model="ruleForm.resource">
+                        <el-radio label="线上品牌商赞助"></el-radio>
+                        <el-radio label="线下场地免费"></el-radio>
+                    </el-radio-group>
+                </el-form-item>
+                <el-form-item
+                    label="活动形式"
+                    prop="desc"
+                    :rules="{ required: true, message: '请填写活动形式', trigger: 'blur' }"
+                >
+                    <el-input type="textarea" v-model="ruleForm.desc"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+                    <el-button @click="resetForm('ruleForm')">重置</el-button>
+                </el-form-item>
+            </el-form>
+            <p>典型表单</p>
+            <el-form ref="formHorizontal" :model="form" label-width="100px" class="project-form-horizontal" label-suffix="：">
+                <el-form-item v-for="item in conts" :key="item" label="活动名称">
+                    <el-input v-model="form.name"></el-input>
+                </el-form-item>
+            </el-form>
+            <p>行内表单</p>
+            <el-form ref="formInline" :model="form" label-width="100px" :inline="true" class="project-form-inline" label-suffix="：">
+                <el-form-item v-for="item in conts" :key="item" label="活动名称">
+                    <el-input v-model="form.name"></el-input>
+                </el-form-item>
+            </el-form>
+            <p>查看详情</p>
+            <el-form ref="formInline" :model="form" label-width="100px" :inline="true" class="project-form-details content-width-180" label-suffix="：">
+                <el-form-item v-for="item in conts" :key="item" label="活动名称">
+                    <p class="project-form-text">{{form.name}}</p>
+                </el-form-item>
+            </el-form>
+        </moc-section>
+    </moc-container>
 </template>
 <script>
     /**
@@ -119,12 +112,7 @@
     export default {
         mixins: [common],
         components: {},
-        props: {
-            visible: {
-                type: Boolean,
-                default: false
-            }
-        },
+        props: {},
         data() {
             return {
                 ruleForm: {
@@ -148,21 +136,12 @@
             }
         },
         created() {
-
-        },
-        mounted() {
             this.initOptions();
         },
+        mounted() {
+        },
         watch: {
-            visible(newValue, oldValue){
-                console.log(newValue)
-                if( newValue === false ){
-                    this.$refs.ruleForm.resetFields();
-                }
-            },
-            groupId(newValue, oldValue){
-                console.log(newValue)
-            }
+
         },
         methods: {
             /**
